9faba4c4b84945172047d4db042cf1925f0269d1
pantobp <nemanjapantovic@live.ac.me>
2015-04-20 11:33:44 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,42 @@
+<?php
+	$adresa[0]="7700 Wadsworth Boulevard"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="Arvada, CO  80003";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Diane Rowland (Fiducial Arvada)";
+	$telephone[0]=3034234040;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="107 Las Palmas"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="Merritt Island, FL  32952";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Gerald K. Atchison";
+	$telephone[1]=3214525854;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="10000 Kings Automall Drive"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="Cincinnati, OH  45249";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Eric Bonney (Fiducial Cincinnati)";
+	$telephone[2]=5132711200;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="2100 East High Street , # T.L.2"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="Springfield, OH  45505";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="Edward L. Comer";
+	$telephone[3]=9373224355;// Broj koji se poziva na dugme CALL US
+
+	$adresa[4]="3181 Sycamore Court"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[4]="Columbus, IN  47203";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[4]="Penny Sue Jones (Fiducial Columbus)";
+	$telephone[4]= 8123783388;// Broj koji se poziva na dugme CALL US
+
+	$adresa[5]="9550 SW Beaverton Hillsdale Hwy"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[5]="Beaverton, OR  97005";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[5]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[5]="Robert Rounds (Fiducial Beaverton)";
+	$telephone[5]=5036437558;// Broj koji se poziva na dugme CALL US
+
+	$centratelephone=3034234040;
+	$companynumber=6;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
